f19b02d72cfcc43b055891e5f788f46281a42b45
berlot83 <berlot83@yahoo.com.ar>
2024-10-10 21:13:50 -0300

    subida de build

---
+++ build/assets/js/a7456010.19815315.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmolokotech_ui=self.webpackChunkmolokotech_ui||[]).push([[1235],{8552:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
---
+++ build/assets/js/aba21aa0.98aaa140.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmolokotech_ui=self.webpackChunkmolokotech_ui||[]).push([[5742],{7093:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
