77c57ed9da027f4d0bcbb17160049a54bbfb3999
berlot83 <berlot83@yahoo.com.ar>
2024-10-11 08:26:34 -0300

    cambios

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -21,8 +21,7 @@ const config: Config = {
   projectName: 'molokotech-blog',
   organizationName: 'berlot83',
   trailingSlash: false,
-  deploymentBranch: "deployment",
-
+  deploymentBranch: "pages",
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
