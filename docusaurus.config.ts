fc30c7796c14857e90306cf1b19022cd2535b4dc
berlot83 <berlot83@yahoo.com.ar>
2024-10-13 08:11:45 -0300

    build

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,9 @@ const config: Config = {
   url: 'https://berlot83.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/molokotech-blog/',
+  
+  // mandar /molokotech-blog/ cuando no se usa dominio custom
+  baseUrl: '/',
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   onBrokenLinks: 'throw',
